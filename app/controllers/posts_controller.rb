dc9b84fdfb82a3a35060725f833b49a12c990efb
hagernofal <hagernofal8@gmail.com>
2026-06-06 16:07:02 +0300

    Complete Posts CRUD (index, show, new, create, edit, update, destroy)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,58 @@
 class PostsController < ApplicationController
   # your code goes here
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts/create
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET /posts/:id/edit
+  def edit
+  end
+
+  # PATCH /posts/:id
+  def update
+    if(@post.update(post_params))
+      redirect_to post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [:title, :content])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
