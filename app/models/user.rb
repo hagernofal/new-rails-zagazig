74661e446988e4da3020f46bcc1afd6c184f45ee
hagernofal <hagernofal8@gmail.com>
2026-06-06 16:18:36 +0300

    Add User resource with validations and email uniqueness constraint

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :dob, presence: true
+    validates :email, presence: true
+    validates :phone_number, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :email, presence: true, uniqueness: true
+
 end
